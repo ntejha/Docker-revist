15c7b9281242710a5b347dca3404ff582044f349
Tejha Nagarajan <ntejha04@gmail.com>
2026-02-03 20:04:35 +0530

    done with terraform

--- terrademo/main.tf
+++ terrademo/main.tf
@@ -1,22 +1,23 @@
 terraform {
   required_providers {
     google = {
-      source = "hashicorp/google"
+      source  = "hashicorp/google"
       version = "7.17.0"
     }
   }
 }
 
 provider "google" {
-  project     = "alert-ground-486213-p9"
-  region      = "asia-south1"
+  credentials = file(var.credentials)
+  project     = var.project
+  region      = var.region
 }
 
 resource "google_storage_bucket" "demo-bucket" {
-  name          = "alert-ground-486213-p9"
-  location      = "ASIA-SOUTH1"
+  name          = var.gcs_bucket_name
+  location      = var.location
   force_destroy = true
-  
+
   lifecycle_rule {
     condition {
       age = 1
@@ -25,4 +26,9 @@ resource "google_storage_bucket" "demo-bucket" {
       type = "AbortIncompleteMultipartUpload"
     }
   }
+}
+
+resource "google_bigquery_dataset" "demo_dataset" {
+  dataset_id = var.bq_dataset_name
+  location   = var.location
 }
